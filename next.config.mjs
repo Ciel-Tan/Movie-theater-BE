1ae3b548f5558b771622bdb786775e7f3e11217a
intern-tan.nd <intern-tan.nd@codecomplete.jp>
2025-03-12 08:18:34 +0700

    try fixing cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
-    const allowedOrigins = [
-      'https://movie-theater-be-api.vercel.app',
-      'http://localhost:3000'
-    ];
-
     return [
       {
-        // Matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: allowedOrigins },
-          { key: "Access-Control-Allow-Methods", value: "GET, DELETE, POST, PUT" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           {
-            key: "Access-Control-Allow-Headers", 
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" 
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
           }
         ]
       }
