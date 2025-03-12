10579fd6758ddbd2ec86ed224c82132add7a3641
intern-tan.nd <intern-tan.nd@codecomplete.jp>
2025-03-12 08:49:54 +0700

    fix cors on vercel ok

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,12 @@ const nextConfig = {
                 { key: "Access-Control-Allow-Credentials", value: "true" },
                 { key: "Access-Control-Allow-Origin", value: "*" },
                 { 
-                    key: "Access-Control-Allow-Methods", 
-                    value: "GET, OPTIONS, PATCH, DELETE, POST, PUT" 
+                  key: "Access-Control-Allow-Methods", 
+                  value: "GET, OPTIONS, PATCH, DELETE, POST, PUT" 
                 },
                 { 
-                    key: "Access-Control-Allow-Headers", 
-                    value: "Authorization, Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version" 
+                  key: "Access-Control-Allow-Headers", 
+                  value: "Authorization, Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version" 
                 },
             ]
         }]
--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,7 @@ async function verifyToken(token) {
     if (!token) {
         return { isValid: false, error: 'No token provided' };
     }
+    
     return { isValid: true };
 }
 
@@ -16,25 +17,22 @@ export async function middleware(request) {
         '/api/accounts/forgot-password',
     ];
 
-    // Allow public routes to pass through
     if (publicRoutes.includes(path) || publicRoutes.some(route => path.startsWith(route))) {
         return NextResponse.next();
     }
 
     if (path.startsWith('/api')) {
-        // Handle OPTIONS preflight requests
         if (request.method === 'OPTIONS') {
             return new NextResponse(null, {
                 status: 200,
                 headers: {
                     'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    'Access-Control-Allow-Methods': 'GET, OPTIONS, PATCH, DELETE, POST, PUT',
                     'Access-Control-Allow-Headers': 'Authorization, Content-Type',
                 },
             });
         }
 
-        // Authentication for other methods
         const authorizationHeader = request.headers.get('Authorization');
         const token = authorizationHeader?.split(' ')[1];
 
